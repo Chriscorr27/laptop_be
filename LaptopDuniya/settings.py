279c05fe5bfb545a349001d93161889bf03736c3
Chris correia <chriscorreia2001@gmail.com>
2021-11-04 19:45:34 +0530

    ip

--- LaptopDuniya/settings.py
+++ LaptopDuniya/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9ln#y89n%ls(=-$z32)5jm0!ckj*_zv7)(eed178c!4e(p4xxk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
